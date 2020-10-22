a53918bb077c39ececaf52badda2084ebff39d55
Nikita <nikita@Nikitas-iMac.fritz.box>
2020-10-22 14:52:57 +0200

    further progress on scrollTrigger animation

--- js/app.js
+++ js/app.js
@@ -1,40 +1,62 @@
-const tlHero = gsap.timeline({
+const tlHero = gsap.timeline({ defaults: { duration: 1, ease: Power4.easeOut } })
+
+tlHero
+  .from("img.logo", { opacity: 0, y: "300%", delay: 1.5 }, 0)
+  .from(".menu-desktop__right a", { opacity: 0, y: 25, stagger: 0.15, delay: 0.25 }, 1)
+  .from(".hero__left__content", { opacity: 0, y: 200, duration: 1.5, delay: 0.75  }, 1)
+  .to(".hero__right img", { opacity: 1, y: 0, duration: 1.5, delay: 0.75 }, 1)
+
+const tlCVImg = gsap.timeline({
+  scrollTrigger: {
+    trigger: "section.cv",
+    start: "top bottom",
+    end:"center center",
+    scrub: true
+  }
+});
+
+tlCVImg
+  .to(".cv__portrait img", { x: 0 })
+
+const tlCV = gsap.timeline({
     scrollTrigger: {
       trigger: ".cv__wrapper",
-      start: "center bottom",
+      start: "top center+=300",
       end: "center top",
       toggleActions: "play none none reverse",
-      markers: true
     }
   });
-  tlHero.to(".cv__portrait img", { opacity: 1, duration: 1 }, 0)
-  tlHero.to("p.hero__left__scroll", { y: "-200%", opacity: 0, duration: 1 }, 0)
-  tlHero.from("div.cv__wrapper p.chapter", { y: 200, opacity: 0, duration: 1 }, 0)
-  tlHero.from("div.cv__wrapper h2.headline", { y: 200, opacity: 0, duration: 1 }, 0)
-  tlHero.from("div.cv__wrapper p.copy", { y: 200, opacity: 0, duration: 1 }, 0)
-  tlHero.from(".cv__cell", { y:"100%", opacity: 0, stagger: 0.1 }, 1)
-  tlHero.from(".cv__logos img", { y:"100%", opacity: 0, stagger: 0.2 }, 2)
+  tlCV.to("p.hero__left__scroll", { y: "-200%", opacity: 0, duration: 1 }, 0)
+  tlCV.from("div.cv__wrapper p.chapter", { y: 200, opacity: 0, duration: 1 }, 0)
+  tlCV.from("div.cv__wrapper h2.headline", { y: 200, opacity: 0, duration: 1 }, 0)
+  tlCV.from("div.cv__wrapper p.copy", { y: 200, opacity: 0, duration: 1 }, 0)
+  tlCV.from(".cv__cell", { y:"100%", opacity: 0, stagger: 0.1 }, 1)
+  tlCV.from(".cv__logos img", { y:"100%", opacity: 0, stagger: 0.2 }, 2)
 
-const tlHeroImg = gsap.timeline({
+const tlKompetenzenImg = gsap.timeline({
   scrollTrigger: {
-    trigger: ,
+    trigger: "section.kompetenzen",
+    start: "top center",
+    end: "bottom center",
+    toggleActions: "play none none reverse",
+    scrub: true
   }
-})
+});
 
-  const tl2 = gsap.timeline({
+tlKompetenzenImg
+  .to(".kompetenzen__portrait img", { x: 0 })
+
+const tlKompetenzen = gsap.timeline({
     scrollTrigger: {
       trigger: ".kompetenzen__wrapper",
       start: "center bottom",
-      end: "center top",
+      end: "center center",
       toggleActions: "play none none reverse",
-      markers: true
     }
   });
-  tl2.set(".hero__right img", {opacity: 0})
-  tl2.to(".kompetenzen__portrait img", {opacity: 1, duration: 1}, 0)
-  tl2.to("body", { backgroundColor: "#F1ECE7", duration: 1 }, 0)
-  tl2.from("div.kompetenzen__wrapper p.chapter", { y: 200, opacity: 0, duration: 1 }, 0)
-  tl2.from("div.kompetenzen__wrapper h2.headline", { y: 200, opacity: 0, duration: 1 }, 0)
-  tl2.from("div.kompetenzen__wrapper p.copy", { y: 200, opacity: 0, duration: 1 }, 0)
-  tl2.from("div.kompetenzen__wrapper h3.kompetenzen__headline", { y: 200, opacity: 0, duration: 1 }, 1)
-  tl2.from(".kompetenzen__cell", { y:"100%", opacity: 0, stagger: 0.1 }, 2)
+  tlKompetenzen.to("body", { backgroundColor: "#F1ECE7", duration: 0.25 }, 0)
+  tlKompetenzen.from("div.kompetenzen__wrapper p.chapter", { y: 200, opacity: 0, duration: 1 }, 0)
+  tlKompetenzen.from("div.kompetenzen__wrapper h2.headline", { y: 200, opacity: 0, duration: 1 }, 0)
+  tlKompetenzen.from("div.kompetenzen__wrapper p.copy", { y: 200, opacity: 0, duration: 1 }, 0)
+  tlKompetenzen.from("div.kompetenzen__wrapper h3.kompetenzen__headline", { y: 200, opacity: 0, duration: 1 }, 1)
+  tlKompetenzen.from(".kompetenzen__cell", { y:"100%", opacity: 0, stagger: 0.1 }, 2)
